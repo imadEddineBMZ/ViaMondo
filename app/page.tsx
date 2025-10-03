54645accda054408df6db3e52a6afbe49ec8d0e4
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2025-10-04 00:15:14 +0100

    Guide section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/hero'
 import Camp from '@/components/camp'
+import Guide from '@/components/guide'
 
 const app = () => {
   return (
     <>
       <Hero />
       <Camp />
+      <Guide />
     </>
   )
 }
---
+++ components/guide.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+const Guide = () => {
+  return (
+    <section className="flexCenter flex-col max-container padding-container border-2 border-red-500">
+      <div className=" w-full pb-24 border-2 border-blue-500">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="Uppercase regular-18 -mt-1 mb-2 lg:mb-4 text-green-50">
+          We are here for you
+        </p>
+      </div>
+      <div className="flex flex-wrap w-full justify-between gap-5 lg:gap-10 border-2 border-green-500">
+        <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+          Guide You to Easy Path
+        </h2>
+        <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+          Only with the hilink application you will no longer get lost and get
+          lost again, because we already support offline maps when there is no
+          internet connection in the field. Invite your friends, relatives and
+          friends to have fun in the wilderness through the valley and reach the
+          top of the mountain
+        </p>
+      </div>
+
+      <div className="max-container w-full my-20 flexCenter relative">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          height={580}
+          width={1440}
+          className="w-full object-cover object-center rounded-2xl "
+        />
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20 ">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col ">
+            <div className="flex flex-col w-full">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-50">Destination</p>
+                <p className="regular-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+            <div className="flex flex-col w-full ">
+              <p className="regular-16 text-gray-50">Start track</p>
+              <p className="bold-20 mt-2">Wonorejo Pasuruan</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Guide;
